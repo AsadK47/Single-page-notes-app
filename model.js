790f055acfa1883c4fda8891eae25281c12b9fb1
Asad Khan <asad_khan47@hotmail.co.uk>
2018-12-11 16:40:35 +0000

    Needs to be refactored into model view controller

--- model.js
+++ model.js
@@ -1,4 +1,4 @@
 function submitMe() {
   var value = document.getElementById('myText').value;
-  document.getElementById('outer').innerHTML = value;
+  document.getElementById('outer').innerHTML += "<br>" + value ;
 }
